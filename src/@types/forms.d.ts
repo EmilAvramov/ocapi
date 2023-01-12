5a0514ec8f2793a9d53033edcc0fb5e5c7599617
Emil Avramov <e_avramov@zoho.eu>
2023-01-12 17:30:14 +0200

    added address form validation

---
+++ src/@types/forms.d.ts
@@ -0,0 +1,13 @@
+declare module '@form-types' {
+    interface AddressForm {
+        email: string;
+        firstName: string;
+        lastName: string;
+        address: string;
+        country: string;
+        state: string;
+        city: string;
+        zip: string;
+        phone: string;
+    }
+}
--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,140 +1,149 @@
-import { Flex, FormControl, Input, Button, Select } from '@chakra-ui/react';
+import { Flex, FormControl, Input, Button, Select, Box } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { emailPattern, states } from '../../config/staticData';
+import * as Yup from 'yup'
+import { AddressForm } from '@form-types';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 export const Checkout: React.FC = (): JSX.Element => {
-	const states = [
-		'Alabama',
-		'Alaska',
-		'Arizona',
-		'Arkansas',
-		'California',
-		'Colorado',
-		'Connecticut',
-		'Delaware',
-		'Florida',
-		'Georgia',
-		'Hawaii',
-		'Idaho',
-		'Illinois',
-		'Indiana',
-		'Iowa',
-		'Kansas',
-		'Kentucky',
-		'Louisiana',
-		'Maine',
-		'Maryland',
-		'Massachusetts',
-		'Michigan',
-		'Minnesota',
-		'Mississippi',
-		'Missouri',
-		'Montana',
-		'Nebraska',
-		'Nevada',
-		'New Hampshire',
-		'New Jersey',
-		'New Mexico',
-		'New York',
-		'North Carolina',
-		'North Dakota',
-		'Ohio',
-		'Oklahoma',
-		'Oregon',
-		'Pennsylvania',
-		'Rhode Island',
-		'South Carolina',
-		'South Dakota',
-		'Tennessee',
-		'Texas',
-		'Utah',
-		'Vermont',
-		'Virginia',
-		'Washington',
-		'West Virginia',
-		'Wisconsin',
-		'Wyoming',
-		'Non-US/Other',
-	];
 
-	const renderOptions = states.map((state) => (
-		<option value={state}>{state}</option>
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().matches(emailPattern, {message: 'Enter a valid email'}).required('Field is required'),
+        firstName: Yup.string().required('Field is required'),
+        lastName: Yup.string().required('Field is required'),
+        address: Yup.string().required('Field is required'),
+        country: Yup.string().required('Field is required'),
+        state: Yup.string().required('Field is required'),
+        city: Yup.string().required('Field is required'),
+        zip: Yup.string().test('len', 'Must be 5 characters long', val => val?.length === 5).required('Field is required'),
+        phone: Yup.string().test('len', 'Must be 10 characters long', val => val?.length === 10).required('Field is required'),
+    })
+
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<AddressForm>({
+        resolver: yupResolver(validationSchema),
+        mode: 'onBlur',
+        reValidateMode: 'onChange'
+    });
+
+	const onSubmit = (data:any) => {
+		console.log(data);
+	};
+
+	const renderOptions = states.map((state, index) => (
+		<option key={index} value={state}>{state}</option>
 	));
+
+
 
 	return (
 		<Flex
-			flexDirection='row'
 			align='center'
 			justifyContent='center'
-            minHeight='79vh'>
-			<form>
+			minHeight='79vh'
+            direction='column'
+            gap='50px'>
+            <Box border='1px solid red' padding='5px' borderRadius='3px'>Shipping/Billing Address Details</Box>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<Flex
 					gap='15px'
 					direction='column'>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='email'
 							flexBasis='80%'
 							type='email'
 							placeholder='Email Address'
+                            {...register('email')}
 						/>
+                        {errors.email && <Box>{errors.email.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='firstName'
 							flexBasis='80%'
 							type='text'
 							placeholder='First Name'
+                            {...register('firstName')}
 						/>
+                        {errors.firstName && <Box>{errors.firstName.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='lastName'
 							flexBasis='80%'
 							type='text'
 							placeholder='Last Name'
+                            {...register('lastName')}
 						/>
+                        {errors.lastName && <Box>{errors.lastName.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='address'
 							flexBasis='80%'
 							type='text'
 							placeholder='Address'
+                            {...register('address')}
 						/>
+                        {errors.address && <Box>{errors.address.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Select
+                            id='country'
 							flexBasis='80%'
-							placeholder='Select Country'>
+							placeholder='Select Country'
+                            {...register('country')}>
 							<option value='USA'>United States</option>
 						</Select>
+                        {errors.country && <Box>{errors.country.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Select
+                            id='state'
 							flexBasis='80%'
-							placeholder='Select State'>
+							placeholder='Select State'
+                            {...register('state')}>
 							{renderOptions}
 						</Select>
+                        {errors.state && <Box>{errors.state.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='city'
 							flexBasis='80%'
 							type='text'
 							placeholder='City'
+                            {...register('city')}
 						/>
+                        {errors.city && <Box>{errors.city.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='zip'
 							flexBasis='80%'
-							type='number'
+							type='text'
 							placeholder='ZIP Code'
+                            {...register('zip')}
 						/>
+                        {errors.zip && <Box>{errors.zip.message}</Box>}
 					</FormControl>
 					<FormControl flexBasis='75%'>
 						<Input
+                            id='phone'
 							flexBasis='80%'
-							type='tel'
+							type='text'
 							placeholder='Phone Number'
+                            {...register('phone')}
 						/>
+                        {errors.phone && <Box>{errors.phone.message}</Box>}
 					</FormControl>
 					<Button
 						flexBasis='75%'
-                        lineHeight='5vh'
+						lineHeight='5vh'
 						type='submit'>
 						Next
 					</Button>
